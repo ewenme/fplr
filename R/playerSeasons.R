d6d6b2a03c6831037458cff933d669de26853158
ewen_ <ewenhenderson@gmail.com>
2017-10-17 23:36:20 +0100

    update player seasons fun in case of no historic data

--- R/playerSeasons.R
+++ R/playerSeasons.R
@@ -8,37 +8,41 @@
 #' playerSeasons(player_id = 54)
 
 playerSeasons <- function(player_id) {
-    
+
     # check the input is numeric, stop if not
     player_id <- as.numeric(player_id)
-    
+
     # get player list
     players <- jsonlite::read_json("https://fantasy.premierleague.com/drf/bootstrap-static", simplifyVector = TRUE)
-    
+
     # check the input is in range, stop if not
-    if (!player_id %in% 1:length(players$elements$id)) 
+    if (!player_id %in% 1:length(players$elements$id))
         stop("player_id not in range.")
-    
+
     # read in json player data, simplify vectors to make easy transfer to dataframe
     data <- jsonlite::read_json(paste0("https://fantasy.premierleague.com/drf/element-summary/", player_id), simplifyVector = TRUE)
-    
+
+    # check if player has season history, stop if not
+    if(nrow(data$history_past) == 0)
+      stop("player_id has no historic data.")
+
     # extract current seasons data ONLY, convert to tibble format
     data <- tibble::as.tibble(data$history_past)
-    
+
     # convert var types
     data$influence <- as.numeric(data$influence)
     data$creativity <- as.numeric(data$creativity)
     data$threat <- as.numeric(data$threat)
     data$ict_index <- as.numeric(data$ict_index)
-    
+
     # convert values to fpl-familiar style
     data$start_price <- data$start_cost/10
     data$end_price <- data$end_cost/10
-    
+
     # append player id
     data$player_id <- player_id
-    
+
     data <- subset(data, select = -c(start_cost, end_cost))
-    
+
     return(data)
 }
