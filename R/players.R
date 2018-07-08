42144828e18b43aa2483d44fe3daa78fb641dd61
ewen_ <ewenhenderson@gmail.com>
2018-07-08 18:30:40 +0100

    combine detailed/historic player funcs

--- R/players.R
+++ R/players.R
@@ -40,9 +40,8 @@ fpl_get_players <- function() {
 
 #' Retrieve detailed data for a player in the current FPL season
 #'
-#' Retrieve detailed (gameweek-level) data for a player in the current FPL season, obtained via the
+#' Retrieve detailed data for a player in the current FPL season, obtained via the
 #' \href{https://fantasy.premierleague.com/drf/bootstrap-static}{bootstrap-static JSON}.
-#' N.B. the current season must have started for this function to return data.
 #'
 #' @param player_id \code{id} field from \code{\link{fpl_get_players}}.
 #'
@@ -52,8 +51,7 @@ fpl_get_players <- function() {
 #'
 #' @examples
 #' \dontrun{
-#' fpl_get_player_detailed(player_id = 1)
-#' fpl_get_player_detailed(player_id = 54)
+#' fpl_get_player_detailed(player_id = 2)
 #' }
 
 fpl_get_player_detailed <- function(player_id) {
@@ -68,75 +66,39 @@ fpl_get_player_detailed <- function(player_id) {
   # read in json player data, simplify vectors to make easy transfer to dataframe
   player_summary <- jsonlite::read_json(paste0(fpl_player_summary, player_id), simplifyVector = TRUE)
 
+  # check if player has season history
+  if(length(player_summary$history_past) >= 1) {
+
+    # extract historic seasons data ONLY
+    player_summary_history <- player_summary$history_past
+
+    # convert numeric vars to numeric class
+    numeric_vars <- c("influence", "creativity", "threat", "ict_index")
+    player_summary_history[numeric_vars] <- sapply(player_summary_history[numeric_vars], as.numeric)
+
+    # convert prices to fpl-familiar denomination
+    player_summary_history[c("start_cost", "end_cost")] <- lapply(player_summary_history[c("start_cost", "end_cost")],
+                                                                  function(x) x / 10)
+
+    # re-add history past
+    player_summary$history_past <- player_summary_history
+  }
+
   # extract current seasons data
-  player_summary <- player_summary$history
+  player_summary_history <- player_summary$history
 
-  if (length(player_summary) < 1)
-    stop("No data for the current season.")
+  if (length(player_summary_history) >= 1) {
 
-  # replace codes with matching values
-  player_summary$opponent_team <- with(players$teams, name[match(player_summary$opponent_team, id)])
+    # replace codes with matching values
+    player_summary_history$opponent_team <- with(players$teams, name[match(player_summary_history$opponent_team, id)])
 
-  # convert price values to fpl-familiar denomination
-  player_summary$value <- player_summary$value / 10
+    # convert price values to fpl-familiar denomination
+    player_summary_history$value <- player_summary_history$value / 10
 
-  # convert numeric vars to numeric class
-  numeric_vars <- c("influence", "creativity", "threat", "ict_index")
-  player_summary[numeric_vars] <- sapply(player_summary[numeric_vars], as.numeric)
+    # convert numeric vars to numeric class
+    numeric_vars <- c("influence", "creativity", "threat", "ict_index")
+    player_summary_history[numeric_vars] <- sapply(player_summary_history[numeric_vars], as.numeric)
+  }
 
-  return(tibble::as_tibble(player_summary))
-
-}
-
-
-#' Retrieve historic seasons summary data for a player in the current FPL season
-#'
-#' Retrieve summary (season-level) data for a player in the current FPL season, for all
-#' previous FPL seasons, obtained via the
-#' \href{https://fantasy.premierleague.com/drf/bootstrap-static}{bootstrap-static JSON}.
-#' N.B. the player must have competed in a previous FPL season for this function to return
-#' data.
-#'
-#' @param player_id \code{id} field from \code{\link{fpl_get_players}}.
-#'
-#' @return a tibble
-#'
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' fpl_get_player_seasons(player_id = 3)
-#' }
-
-fpl_get_player_seasons <- function(player_id) {
-
-  # get player list
-  players <- fpl_get_bootstrap()
-
-  # check the input is in range, stop if not
-  if (!player_id %in% 1:length(players$elements$id))
-    stop("player_id out of range.")
-
-  # read in json player data, simplify vectors to make easy transfer to dataframe
-  player_summary <- jsonlite::read_json(paste0(fpl_player_summary, player_id), simplifyVector = TRUE)
-
-  # check if player has season history, stop if not
-  if(length(player_summary$history_past) == 0)
-    stop("player_id has no historic data.")
-
-  # extract historic seasons data ONLY
-  player_summary <- player_summary$history_past
-
-  # convert numeric vars to numeric class
-  numeric_vars <- c("influence", "creativity", "threat", "ict_index")
-  player_summary[numeric_vars] <- sapply(player_summary[numeric_vars], as.numeric)
-
-  # convert prices to fpl-familiar denomination
-  player_summary[c("start_cost", "end_cost")] <- lapply(player_summary[c("start_cost", "end_cost")],
-                                                        function(x) x / 10)
-
-  # append player id
-  player_summary$element <- player_id
-
-  return(tibble::as_tibble(player_summary))
+  return(player_summary)
 }
